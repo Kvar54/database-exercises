867f868671afff3b63d9297da11e5e705cb674cc
Keila Vargas <keila.vargas0365@gmail.com>
2023-08-18 12:09:33 -0500

    completed problem #3

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,24 @@
 CREATE DATABASE IF NOT EXISTS codeup_test_db;
 
 USE codeup_test_db;
+
+SELECT * FROM albums
+WHERE artist = 'pink floyd';
+
+SELECT release_date FROM albums
+WHERE artist = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT genre FROM albums
+WHERE name = ' Nevermind';
+
+SELECT * FROM albums
+WHERE release_date BETWEEN '1990' AND '1999';
+
+SELECT * FROM albums
+WHERE sales < '20.0';
+
+SELECT * FROM albums
+WHERE genre < 'Rock';
+
+
+
