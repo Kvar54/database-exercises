345f7c8fe2dfcf0a0ff8695eb8969ceee129403e
Keila Vargas <keila.vargas0365@gmail.com>
2023-08-18 11:53:06 -0500

    inserted data in albums table

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE IF NOT EXISTS albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    release_date INT NOT NULL,
+    sales DECIMAL(8,2) NOT NULL,
+    genre VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
---
+++ select_exercises.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS codeup_test_db;
+
+USE codeup_test_db;
